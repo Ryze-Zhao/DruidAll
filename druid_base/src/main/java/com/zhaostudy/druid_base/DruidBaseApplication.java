fc6ef34175e5330643b652ddcf700d4d26a7f4c9
Ryze-Zhao <643080112@qq.com>
2019-08-18 09:06:01 +0800

    创建SpringBoot

---
+++ druid_base/src/main/java/com/zhaostudy/druid_base/DruidBaseApplication.java
@@ -0,0 +1,13 @@
+package com.zhaostudy.druid_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidBaseApplication.class, args);
+    }
+
+}
